ae03851dce4c9caeb4004b443462f589bd637a52
Ryan Zemlin <rzemlin@gmail.com>
2023-11-25 10:46:01 -0800

    add static components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './App.css'
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,7 +22,86 @@ const initialFriends = [
 ];
 
 function App() {
-  return <div>Hello</div>
+  
+  return <div className='app'>
+    <div className='sidebar'>
+    <FriendsList />
+    <FormAddFriend />
+    <Button>Add Friend</Button>
+    </div>
+    <FormSplitBill />
+   
+  </div>
 }
 
+function FriendsList(){
+  const friends= initialFriends;
+  return (
+   <ul>
+    {friends.map((friend) => (
+    <Friend friend={friend} key={friend.id} />
+    ))}
+  </ul>
+  );
+}
+
+function Friend({friend}) {
+  return (
+  <li>
+   <img src={friend.image} alt={friend.name}/>
+   <h3>{friend.name}</h3>
+  
+  {friend.balance < 0 && ( 
+  <p className='red'>
+    You owe {friend.name} ${Math.abs(friend.balance)} 😩
+    </p> )}
+    {friend.balance > 0 && ( 
+  <p className='green'>
+    {friend.name} owes you ${Math.abs(friend.balance)} 🤗
+    </p> )}
+    {friend.balance === 0 && ( 
+  <p>
+    You and {friend.name} are even 🙃
+    </p> )}
+    <Button>Select</Button>
+  </li>
+)}
+
+function FormAddFriend() {
+  return (
+  <form className='form-add-friend'>
+    <label>👬 Add Friend</label>
+    <input type='text' />
+    <label>🙊 Friend URL</label>
+    <input type='text' placeholder='www.some-site.com'/>
+    <Button>Add</Button>
+  </form>
+  )
+}
+
+function Button({children}){
+  return (
+<button className='button'>{children}</button>
+  )
+}
+
+function FormSplitBill() {
+  return (
+    <form className='form-split-bill'>
+      <h2>Split a bill with x</h2>
+      <label>💰 Total Bill</label>
+    <input type='text' />
+    <label>🕺 Your Share</label>
+    <input type='text' />
+    <label>🥳 x's share</label>
+    <input type='text' disabled/>
+    <label>🤑Who's Paying?</label>
+    <select>
+    <option value='user'>You</option>
+    <option value='friend'>X</option>
+    </select>
+    <Button>Split Bill</Button>
+      </form>
+  )
+}
 export default App;
